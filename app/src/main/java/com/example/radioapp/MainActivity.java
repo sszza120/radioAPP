b79ff91a349cbf64f3cf06b3d0ed00f41702a7f1
sszza120 <z875ab@gmail.com>
2020-03-30 10:45:12 +0800

    03301043

--- app/src/main/java/com/example/radioapp/MainActivity.java
+++ app/src/main/java/com/example/radioapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn_ok(View view) {
+        TextView txtLike = findViewById(R.id.txtShow);
+        RadioGroup kGirlGroup =findViewById(R.id.kGirlGroup);
+        int id = kGirlGroup.getCheckedRadioButtonId();
+        RadioButton selected = findViewById(id);
+        txtLike.setText("你最喜歡的韓國女團是?:" + selected.getText());
     }
 }
